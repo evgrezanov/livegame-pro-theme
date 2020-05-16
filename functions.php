e92d89479ea383b3e31449758882142c2a9fae95
Evgeniy Rezanov <evgrezanov@gmail.com>
2020-05-16 17:21:21 +0700

    branches

--- functions.php
+++ functions.php
@@ -41,6 +41,7 @@ add_shortcode( 'site_year', 'site_year' );
 // Your code goes below
 //
 
+// link featured images
 function change_custom_post_type_process() {
 	register_batch_process( array(
 		'name' => 'Change features image for teams',
@@ -59,4 +60,67 @@ function process_feature_img_change( $post ) {
     $media = array_shift( $media );
     $image_id = $media->ID;
     set_post_thumbnail( $post->ID, $image_id );
+}
+
+// new columns 
+function get_all_sports_leagues() {
+	register_batch_process( array(
+		'name' => 'Add team id columns to custom tables',
+		'type' => 'term',
+		'args' => array(
+            'taxonomy' => 'sports',
+            'exclude'   => array(2,4),
+		),
+		'callback' => 'lgp_add_new_columns',
+	) );
+}
+add_action( 'locomotive_init', 'get_all_sports_leagues' );
+
+function lgp_add_new_columns( $category ) {
+    global $wpdb;
+    $query = "ALTER TABLE " . $wpdb->prefix . $category->slug . " ADD team1_id int(10)";
+    $res = $wpdb->query($query);
+    
+    $query2 = "ALTER TABLE " . $wpdb->prefix . $category->slug . " ADD team2_id int(10)";
+	$res2 = $wpdb->query($query2);
+
+}
+
+// link teams
+function add_team_id_to_custom_table() {
+	register_batch_process( array(
+		'name' => 'Link teams to custom tables',
+		'type' => 'post',
+		'args' => array(
+            'post_type' => 'teams',
+            'posts_per_page' => -1,
+		),
+		'callback' => 'process_add_team_id',
+	) );
+}
+add_action( 'locomotive_init', 'add_team_id_to_custom_table' );
+
+function process_add_team_id( $post ) {
+    global $wpdb;
+    $cur_terms = get_the_terms( $post, 'sports' );
+    if( is_array( $cur_terms ) ){
+        foreach( $cur_terms as $cur_term ){
+            if ($cur_term->slug != 'hokkey' && $cur_term->slug != 'soccer'):
+                $t1res = $wpdb->update( 
+                    $wpdb->prefix.$cur_term->slug,
+	                array( 'team1_id' => $post->ID),
+                    array( 'team1' => $post->post_title ),
+                    array( '%d' ),
+                    array( '%s' )
+                );
+                $t2res = $wpdb->update( 
+                    $wpdb->prefix.$cur_term->slug,
+	                array( 'team2_id' => $post->ID),
+                    array( 'team2' => $post->post_title ),
+                    array( '%d' ),
+                    array( '%s' )
+                );
+            endif;
+        }
+    }
 }
